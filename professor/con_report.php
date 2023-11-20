ee4132731a5706e12128c65da8c8e9c998486df2
moises0410 <148221667+moises0410@users.noreply.github.com>
2023-11-20 13:47:50 +0800

    Add files via upload

---
+++ professor/con_report.php
@@ -0,0 +1,256 @@
+<?php
+session_start();
+
+$recordsPerPage = 5;
+$offset = 0; 
+$page = 1; 
+
+if (isset($_GET["page"]) && is_numeric($_GET["page"])) {
+    $page = $_GET["page"];
+    $offset = ($page - 1) * $recordsPerPage; 
+}
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    
+    $hostname = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "chatapp";
+
+
+    $conn = new mysqli($hostname, $username, $password, $database);
+
+
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+
+    $name = $_POST["search"];
+
+
+    $query = "SELECT name, stud_no, level, concern FROM consult WHERE name = ?";
+    $stmt = $conn->prepare($query);
+    $stmt->bind_param("s", $name);
+    $stmt->execute();
+    $stmt->bind_result($name, $stud_no, $level, $concern);
+
+    if ($stmt->fetch()) {
+ 
+
+        $stmt->close();
+
+        $remark = $_POST["remark"];
+
+        $insertQuery = "INSERT INTO consult_report (name, stud_no, level, concern, remark) VALUES (?, ?, ?, ?, ?)";
+        $insertStmt = $conn->prepare($insertQuery);
+        $insertStmt->bind_param("sssss", $name, $stud_no, $level, $concern, $remark);
+
+        if ($insertStmt->execute()) {
+            echo "Remark successfully inserted.";
+        } else {
+            echo "Error inserting remark: " . $conn->error;
+        }
+
+        $insertStmt->close();
+    } else {
+        echo "Student with the provided student number not found.";
+    }
+
+    $conn->close();
+}
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Consultation Report</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f4f4f4;
+            margin: 0;
+            padding: 0;
+        }
+
+        .container-report {
+            width: 500px;
+            float: left;
+            margin-right: 20px;
+            padding: 20px;
+            background-color: #fff;
+            border-radius: 5px;
+            box-shadow: 0 0 5px rgba(0, 0, 0, 0.2);
+        }
+
+        .container-table {
+            width: 650px;
+            float: left;
+            padding: 1px;
+            background-color: #fff;
+            border-radius: 0px;
+            box-shadow: 0 0 5px rgba(0, 0, 0, 0.2);
+        }
+
+        .student-table {
+            width: 100%;
+            border-collapse: collapse;
+        }
+
+        .student-table th {
+            background-color: lightblue;
+            text-align: center;
+            padding: 5px;
+        }
+
+        .student-table td {
+            border: 0px solid #ccc;
+            padding: 5px;
+        }
+
+        .pagination {
+            margin-top: 20px;
+        }
+
+        .pagination a {
+            display: inline-block;
+            padding: 5px 10px;
+            margin-right: 5px;
+            border: 1px solid #ccc;
+            text-decoration: none;
+            background-color: lightblue;
+            color: #333;
+            border-radius: 5px;
+        }
+
+        .pagination a:hover {
+            background-color: #555;
+            color: #fff;
+        }
+
+        h2 {
+            text-align: center;
+            color: #333;
+        }
+
+        form {
+            text-align: left;
+        }
+
+        .form-group {
+            margin-bottom: 10px;
+        }
+
+        label {
+            display: block;
+            font-weight: bold;
+        }
+
+        input[type="text"] {
+            width: 95%;
+            padding: 8px;
+            margin-bottom: 10px;
+            border: 1px solid #ccc;
+            border-radius: 3px;
+        }
+
+        textarea {
+            width: 95%;
+            height: 150px;
+            padding: 8px;
+            margin-bottom: 10px;
+            border: 1px solid #ccc;
+            border-radius: 3px;
+        }
+
+        input[type="submit"] {
+            background-color: #333;
+            color: #fff;
+            padding: 10px 15px;
+            border: none;
+            border-radius: 3px;
+            cursor: pointer;
+        }
+
+        input[type="submit"]:hover {
+            background-color: #555;
+        }
+    </style>
+</head>
+
+<body>
+    <br>
+
+    <div class="container-report">
+        <h2>Consultation Report</h2>
+        <form method="post" action="">
+            <div class="form-group">
+                <label for="search">Student Name:</label>
+                <input type="text" id="search" name="search">
+            </div>
+            <div class="form-group">
+                <label for="remark">Remark:</label>
+                <textarea id="remark" name="remark"></textarea>
+            </div>
+            <div class="form-group">
+                <input type="submit" value="Submit">
+            </div>
+        </form>
+    </div>
+
+    <div class="container-table">
+        <h2>Consultation Data</h2>
+        <?php
+        $hostname = "localhost";
+        $username = "root";
+        $password = "";
+        $database = "chatapp";
+
+        // Create a database connection
+        $conn = new mysqli($hostname, $username, $password, $database);
+
+        $totalRecordsQuery = "SELECT COUNT(*) AS total_records FROM consult_report";
+        $totalRecordsResult = $conn->query($totalRecordsQuery);
+        $totalRecords = $totalRecordsResult->fetch_assoc()['total_records'];
+
+        // Calculate the total number of pages
+        $totalPages = ceil($totalRecords / $recordsPerPage);
+
+        // Query to fetch a specific range of records based on pagination
+        $tableQuery = "SELECT * FROM consult_report LIMIT $recordsPerPage OFFSET $offset";
+        $tableResult = $conn->query($tableQuery);
+
+        if ($tableResult->num_rows > 0) {
+            echo "<table class='student-table'>";
+            echo "<tr><th>Name</th><th>Student Number</th><th>Year & Course</th><th>Area of Concern</th><th>Remark</th></tr>";
+            while ($row = $tableResult->fetch_assoc()) {
+                echo "<tr><td>" . $row["name"] . "</td><td>" . $row["stud_no"] . "</td><td>" . $row["level"] . "</td><td>" . $row["concern"] . "</td><td>" . $row["remark"] . "</td></tr>";
+            }
+            echo "</table>";
+        } else {
+            echo "No student records found.";
+        }
+
+    
+        if ($totalPages > 1) {
+            echo "<div class='pagination'>";
+            if ($page > 1) {
+                echo "<a href='?page=" . ($page - 1) . "'>Previous</a>";
+            }
+            for ($i = 1; $i <= $totalPages; $i++) {
+                echo "<a href='?page=" . $i . "'>$i</a>";
+            }
+            if ($page < $totalPages) {
+                echo "<a href='?page=" . ($page + 1) . "'>Next</a>";
+            }
+            echo "</div>";
+        }
+        ?>
+    </div>
+</body>
+
+</html>
+
+<?php
+$conn->close();
+?>

43f38c77c45cb9723716e55b5b9c874350479484
moises0410 <148221667+moises0410@users.noreply.github.com>
2023-12-07 07:38:58 +0800

    Add files via upload

---
+++ professors/reports_may.php
@@ -0,0 +1,105 @@
+<?php
+
+include '../config.php';
+session_start();
+$unique_id = $_SESSION['unique_id'];
+$user_id = $_SESSION['user_id'];
+
+if(!isset($_SESSION['user_id'])){
+   header('location:../login.php');
+};
+if(!isset($unique_id)){
+    header('location:../index.php');
+};
+
+if(isset($_GET['logout'])){
+   unset($user_id);
+   session_destroy();
+   header('location:../login.php');
+}
+
+?>
+<!DOCTYPE html>
+
+<html lang="en">
+
+  <head>
+
+    <meta charset="UTF-8" />
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <link
+
+      href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap"
+
+      rel="stylesheet"
+
+    />
+    <link rel="stylesheet" href="css/styler.css" />
+
+    <link rel="stylesheet" href="css/styles.css" />
+
+    <link rel="stylesheet" href="css/style.css" />
+    
+
+    <title>Home</title>
+
+  </head>
+
+  <body>
+
+    <nav>
+
+      <div class="head">
+
+        <h4>PROFESSOR</h4>
+
+      </div>
+
+      <ul class="nav-links">
+
+        <li><a href="may.php">Home</a></li>
+
+        <li><a href="consult_may.php">Consultation</a></li>
+
+        <li><a class="active"  href="reports_may.php">Report</a></li>
+
+        <li><a href="chat_may/users.php">Message</a></li>
+
+
+        <?php 
+            $sql = mysqli_query($conn, "SELECT * FROM users WHERE unique_id = {$_SESSION['unique_id']}");
+            if(mysqli_num_rows($sql) > 0){
+              $row = mysqli_fetch_assoc($sql);
+            }
+          ?>
+        <li><a href="../logout.php?logout_id=<?php echo $row['unique_id']; ?>" class="logout">Logout</a></li>
+
+      </ul>
+
+    </nav>
+
+       
+    <iframe src="report/may.php" height="700" width="1500" frameborder = "0"></iframe>
+
+<section class="footer">
+ <div class="box-container">
+     <div class="box">
+     <?php
+      $select = mysqli_query($conn, "SELECT * FROM `users` WHERE user_id = '$user_id'") or die('query failed');
+      if(mysqli_num_rows($select) > 0){
+         $fetch = mysqli_fetch_assoc($select);
+      }
+   ?>
+         <h2>You are logged in as <?php echo $fetch['fname']; ?> <?php echo $fetch['lname']; ?></h2>
+         <a href="may.php">HOME</a>
+         <a href="../logout.php?logout_id=<?php echo $row['unique_id']; ?>" class="logout">LOGOUT</a>
+     </div>
+ </div>
+
+ <div class="credit"> copyright @ 2023</div>
+
+  </body>
+
+</html>
